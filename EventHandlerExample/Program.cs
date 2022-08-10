c937417ecaa4b16bdb61820648530430ec19e2f9
Pedro André dos Santos <pedro.santos@havan.com.br>
2022-08-10 10:03:59 -0300

    first commit

---
+++ EventHandlerExample/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace EventHandlerExample
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pressione a tecla P");
+            var p = Console.ReadLine();
+            if (p == "p")
+            {
+                KeyPressed();
+            }
+        }
+
+        static void KeyPressed()
+        {
+            Button button = new Button();
+            button.OnClickEvent += (s, args) =>
+            {
+                Console.WriteLine($"{args.Name}, Você clicou no botão");
+            };
+            button.Click();
+        }
+    }
+
+    public class Button
+    {
+        public event EventHandler<MyCustomArguments>? OnClickEvent;
+        MyCustomArguments customArguments = new MyCustomArguments();
+        public void Click()
+        {
+            customArguments.Name = "Pedro";
+            OnClickEvent?.Invoke(this, customArguments);
+        }
+    }
+
+    public class MyCustomArguments : EventArgs
+    {
+        public string? Name { get; set; }
+    }
+}
